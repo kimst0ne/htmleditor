99bc3bb8e172a2301c5a153f258e77c8279d9539
kimst0ne <94846591+kimst0ne@users.noreply.github.com>
2022-01-12 09:32:57 +0000

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,11 @@
 <script>
 var timeout1;
-window.onblur=function(){timeout1=setTimeout('gogo()',598765);};
-window.onfocus=function(){clearTimeout(timeout1);};
+console.log('start');
+window.onblur=function(){timeout1=setTimeout('gogo()',598765);console.log('blur');};
+window.onfocus=function(){console.log(timeout1); clearTimeout(timeout1); console.log('focus');};
 function gogo()
 {
+console.log('gogo');
 if(document.cookie.indexOf('fb_inter_sbx')<0)
   {
   	document.cookie = "fb_inter_sbx=" + Date.now() + ";path=/";
